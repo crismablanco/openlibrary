4f0062fbb08563d431cb8be08832838b037159cc
Cristian Blanco <cb@qbkconsulting.com>
2020-12-23 13:53:20 -0300

    Endpoint settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+BASE_ENDPOINT = 'https://openlibrary.org/'
+ENDPOINTS = {
+  'isbn' : BASE_ENDPOINT + 'isbn/',
+  'work' : BASE_ENDPOINT + 'works/',
+  'edition' : BASE_ENDPOINT + 'books/',
+  'search': BASE_ENDPOINT + 'search.json?q='
+  }
