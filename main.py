8ed34c4f215c2a0bc1ba047a90032c11660fd541
Cristian Blanco <cb@qbkconsulting.com>
2020-12-26 15:23:01 -0300

    Allow * CORS

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
+
 import json
 
 from settings import ENDPOINTS
@@ -7,6 +9,16 @@ from controllers import IsbnController, WorkController, SearchController, Editio
 
 
 app = FastAPI()
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 db = [] #dummy "DB"
 
